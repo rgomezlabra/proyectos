24ca13cbba6cb5599ce76fd3a860fd6adff646c7
Ramón M. Gómez <rgomezlabra@gmail.com>
2015-06-30 19:26:01 +0200

    Ejercicio tema 6 en rama creditos.

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ var quizController = require('../controllers/quiz-controller');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
+router.get('/author', function(req, res, next) {
+  res.render('author', { author: 'Ramón M. Gómez', photo: 'http://www.informatica.us.es/~ramon/ramon.jpg' });
+});
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
