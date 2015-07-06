802236d3c706db34dec6dc6fd1f9a994ca44de12
Ramón M. Gómez <rgomezlabra@gmail.com>
2015-07-06 19:47:12 +0200

    Usar BD SQLite para preguntas.

---
+++ models/models.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var Sequelize = require('sequelize');
+
+// Acceso a BD.
+// PostgreSQL: DATABASE_URL=postgres://usuario:clave@servidor:puerto/bd/
+// SQLite:     DATABASE_URL=sqlite://:@:/
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user    = (url[2]||null);
+var pwd     = (url[3]||null);
+var protocol= (url[1]||null);
+var port    = (url[5]||null);
+var host    = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
+// Elegir motor de BD.
+var sequelize = new Sequelize(DB_name, user, pwd,
+    { dialect: protocol, protocol: protocol, port: port, host: host,
+      storage: storage, omitNull: true });
+
+// Importar definición de la tabla.
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+exports.Quiz = Quiz;
+
+// Inicializar la BD de preguntas.
+sequelize.sync().success(function() {
+	Quiz.count().success(function(count) {
+		if (count === 0) {
+			Quiz.create({ pregunta: 'Capital de Italia', respuesta: 'Roma' })
+			    .success(function() {
+				console.log('Base de datos inicializada.')
+			    });
+		}
+	});
+});
